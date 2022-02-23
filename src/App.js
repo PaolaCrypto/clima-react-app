3d24b993ffe970a557727426f9335dcdc912c18f
PaolaCrypto <paolaseverino@protonmail.com>
2022-02-23 15:08:41 -0500

    adding my github link and bootsrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h2>Weather App</h2>
+      <footer>
+        This project was coded by Paola Severino and is{" "}
+        <a
+          href="https://github.com/PaolaCrypto/clima-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
