77750d3c54273fff12fdbbedb598517a3029f565
PaolaCrypto <paolaseverino@protonmail.com>
2022-02-25 18:24:48 -0500

    create html app structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h2>Weather App</h2>
-      <footer>
-        This project was coded by Paola Severino and is{" "}
-        <a
-          href="https://github.com/PaolaCrypto/clima-react-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h2>Weather App</h2>
+        <Weather />
+        <footer>
+          This project was coded by Paola Severino and is{" "}
+          <a
+            href="https://github.com/PaolaCrypto/clima-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Santo Domingo</h1>
+      <ul>
+        <li>Viernes 6:00</li>
+        <li>Nublado</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          24°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 93%</li>
+            <li>Humidity: 82%</li>
+            <li>Wind: 14 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
